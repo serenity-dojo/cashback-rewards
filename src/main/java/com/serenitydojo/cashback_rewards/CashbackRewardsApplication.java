9d8e81e581502c0d7d127edd1df7974b07d532bc
John Ferguson Smart <john.smart@wakaleo.com>
2026-04-06 18:29:32 +0100

    Initial project structure

---
+++ src/main/java/com/serenitydojo/cashback_rewards/CashbackRewardsApplication.java
@@ -0,0 +1,13 @@
+package com.serenitydojo.cashback_rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbackRewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashbackRewardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serenitydojo/cashback_rewards/CashbackRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serenitydojo.cashback_rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbackRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
